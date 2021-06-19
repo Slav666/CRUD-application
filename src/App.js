2f96aa47c8eb31e6eaedbb6241ef1884182ca816
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-06-19 12:59:29 +0100

    Add input form with css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="form">
       <h1>CRUD Application</h1>
+      <label>Item Name:</label>
+      <input type="text" name="itemName" />
+      <label>Description:</label>
+      <input type="text" name="description" />
+      <button>Submit</button>
     </div>
   );
 }
